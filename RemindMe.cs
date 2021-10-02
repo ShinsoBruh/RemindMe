c497a26ef057685ad16a4d59121ee35be47d2d08
Cara <git@caraxian.com>
2021-10-02 22:49:40 +0930

    [WAR] Don't show Berserk when level 70 or higher.
    Should probably check that Inner Release is actually unlocked... but oh well

--- RemindMe.cs
+++ RemindMe.cs
@@ -202,6 +202,7 @@ namespace RemindMe {
         private delegate bool ActionSpecialCheckDelegate(MonitorDisplay display, CooldownMonitor cooldownMonitor, DalamudPluginInterface pluginInterface);
 
         private Dictionary<uint, ActionSpecialCheckDelegate> actionSpecialChecks = new Dictionary<uint, ActionSpecialCheckDelegate> {
+            { 38, (_, _, _) => Service.ClientState.LocalPlayer?.Level < 70 },
             { 2872, (_, _, _) => Service.ClientState.LocalPlayer?.Level < 76 },
             { 7400, ((display, monitor, pluginInterface) => {
                 // Nastrond, Only show if in Life of the Dragon
